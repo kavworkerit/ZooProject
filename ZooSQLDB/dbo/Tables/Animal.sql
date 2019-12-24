fb429435f78bd53ca544730f2b353b8d81a5e061
kavworkerit <kav.worker.it@gmail.com>
2019-12-25 00:25:47 +0300

    Создана таблица для БД

---
+++ ZooSQLDB/dbo/Tables/Animal.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Animal] (
+    [Name]         VARCHAR (50)  NOT NULL,
+    [From]         VARCHAR (255) NOT NULL,
+    [Cost]         FLOAT (53)    NOT NULL,
+    [Nickname]     VARCHAR (50)  NOT NULL,
+    [Age]          INT           NOT NULL,
+    [Height]       INT           NOT NULL,
+    [Weight]       INT           NOT NULL,
+    [Gender]       VARCHAR (10)  NOT NULL,
+    [Reproduction] VARCHAR (50)  NOT NULL,
+    [Song]         VARCHAR (255) DEFAULT ('') NOT NULL,
+    [ContentType]  VARCHAR (50)  NOT NULL,
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_Animal] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
